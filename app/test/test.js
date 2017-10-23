06a07a6ccf9f2a07c27b82528ac2d300df171238
jengleberg <jeffengleberg@hotmail.com>
2017-10-23 17:06:27 -0600

    tweaks search

--- app/test/test.js
+++ app/test/test.js
@@ -1,7 +1,8 @@
-
+// Requiring our models and chai module
 var Theory = require("../models/theory");
 var expect = require("chai").expect;
 
+// Test to see if I can create a new Theory.  Passed!!
 describe("Theory", function() {
  describe("new", function() {
   it("initializes a new theory", function() {
